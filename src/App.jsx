81074444e87c1c20f8bab6fdef15dea3599f1a0b
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-09-04 14:26:44 -0300

    cotrol components

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,34 @@ import "./App.css"
 import { useState } from "react"
 
 const App = () => {
-  const [value, setValue] = useState("")
+  const [value, setValue] = useState({ normal: "", texto: "", select: "" })
   const handleChange = (e) => {
-    setValue(e.target.value)
+    console.log(e.target.name)
+    setValue((state) => ({
+      ...state,
+      [e.target.name]: e.target.value,
+    }))
   }
   console.log(value)
 
   return (
     <div>
-      {value.length > 5 ? <p>El valor es mayor a 5</p> : null}
+      {value.length < 5 ? <span>logitud minima de 5 caracteres</span> : null}
       <input
         type="text"
         name="normal"
-        id=""
-        value={value}
+        value={value.normal}
         onChange={handleChange}
+        placeholder="normal"
       />
+      <textarea name="texto" onChange={handleChange} cols="30" rows="2" />
+      <select name="select" value={value.select} onChange={handleChange} id="">
+        <option value="">seleccione</option>
+        <option value="chanchofeliz">chancho feliz</option>
+        <option value="chanchotriste">chancho triste</option>
+        <option value="chanchitofeliz">chanchito feliz</option>
+        <option value="felipe">felipe</option>
+      </select>
     </div>
   )
 }
